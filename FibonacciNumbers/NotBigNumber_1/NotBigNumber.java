0d4cd1ad27b4c67e2e4d81a6eff6a97b45feaf3a
Anastasia Kirillova <anki0216@netcracker.com>
2018-12-24 18:04:05 +0300

    added solutions

---
+++ FibonacciNumbers/NotBigNumber_1/NotBigNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NotBigNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[n]);
+    }
+}
