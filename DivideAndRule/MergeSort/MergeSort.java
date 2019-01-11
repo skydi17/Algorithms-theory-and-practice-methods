21843cd9aeebdf12dabd9985959e5bcca426c918
skydi17 <vlayan@icloud.com>
2019-01-11 15:47:48 +0300

    fixed merge sort

--- DivideAndRule/MergeSort/MergeSort.java
+++ DivideAndRule/MergeSort/MergeSort.java
@@ -2,42 +2,30 @@ import java.util.Scanner;
 
 public class MergeSort {
 
-    public static long numbOfInversions = 0;
+    public static long inversions = 0;
 
-    static long[] mergeSort(long[] arr, int l, int r) {
-        if (l < r) {
-            int m = (l + r) / 2;
-            merge(mergeSort(arr, l, m), mergeSort(arr, m + 1, r));
-        }
-        return arr;
+    static long[] mergeSort(long[] arr) {
+        if (arr.length == 1) return arr;
+        int m = arr.length / 2;
+        long[] left = new long[m];
+        System.arraycopy(arr, 0, left, 0, m);
+        long[] right = new long[arr.length - m];
+        System.arraycopy(arr, m, right, 0, arr.length - m);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
     }
 
     static long[] merge(long[] arr1, long[] arr2) {
-        if (arr1[0] >= arr2[0]) {
-            numbOfInversions = numbOfInversions + arr1.length;
-        }
         long[] res = new long[arr1.length + arr2.length];
-        int i = 0, j = 0, k = 0;
-        while (arr1.length > i && arr2.length > j) {
-            if (arr1[i] > arr2[j]) {
+        int i = 0, j = 0;
+        for (int k = 0; k < res.length; k++) {
+            if (i == arr1.length || (j < arr2.length && arr1[i] > arr2[j])) {
                 res[k] = arr2[j];
                 j++;
+                inversions = inversions + arr1.length - i;
             } else {
                 res[k] = arr1[i];
-                i++;
-            }
-            k++;
-        }
-        if (arr1.length - 1 == i) {
-            while (k < arr2.length) {
-                res[k] = arr2[j];
-                k++;
-                j++;
-            }
-        } else {
-            while (k < arr1.length) {
-                res[k] = arr1[i];
-                k++;
                 i++;
             }
         }
@@ -51,7 +39,7 @@ public class MergeSort {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextLong();
         }
-        mergeSort(arr, 0, arr.length - 1);
-        System.out.println(numbOfInversions);
+        mergeSort(arr);
+        System.out.println(inversions);
     }
 }
