0ea5bb71b225da042e6c88ab94a70f9c8dee45dd
Anastasia Kirillova <anki0216@netcracker.com>
2019-01-15 12:32:17 +0300

    added solution

--- DynamicProgramming/Bagpack/Bagpack.java
+++ DynamicProgramming/Bagpack/Bagpack.java
@@ -3,13 +3,25 @@ import java.util.Scanner;
 public class Bagpack {
 
     static int findMaxWeight(int[] arr, int w) {
-        int[][] d = new int[arr.length + 1][arr.length + 1];
-        for (int i = 0; i <= arr.length; i++) {
-            for (int j = 0; j <= arr.length; j++) {
+        int[][] d = new int[w + 1][arr.length + 1];
 
+        for (int i = 0; i <= arr.length; i++) {
+            d[0][i] = 0;
+        }
+
+        for (int i = 0; i <= w; i++) {
+            d[i][0] = 0;
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            for (int j = 1; j <= w; j++) {
+                d[j][i] = d[j][i - 1];
+                if (arr[i - 1] <= j) {
+                    d[j][i] = Math.max(d[j][i - 1], d[j-arr[i - 1]][i - 1]+arr[i-1]);
+                }
             }
         }
-        return d[arr.length][arr.length];
+        return d[w][arr.length];
     }
 
     public static void main(String[] args) {
