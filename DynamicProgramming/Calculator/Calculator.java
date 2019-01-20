bb28af875e4bf2960f0e5021919e208f0f719fab
skydi17 <vlayan@icloud.com>
2019-01-20 15:03:44 +0300

    added solutions

---
+++ DynamicProgramming/Calculator/Calculator.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Calculator {
+    static ArrayList<Integer> res = new ArrayList<>();
+    static TreeSet set = new TreeSet();
+
+    static int calculate(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n % 3 == 0) {
+            res.add(n/3);
+            return calculate(n/3);
+        }
+        if (n % 2 == 0 && (!set.contains(n))) {
+            res.add(n/2);
+            return calculate(n/2);
+        }
+        res.add(n - 1);
+        return calculate(n - 1);
+    }
+
+    static void fillSet(){
+        set.add(10);
+        set.add(82);
+        set.add(226);
+        set.add(382);
+        set.add(694);
+        set.add(958);
+        set.add(6250);
+        set.add(98734);
+    }
+
+    public static void main(String[] args) {
+        fillSet();
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n == 1) {
+            System.out.println("0\n1");
+        } else {
+            /*res.add(n);
+            calculate(n);
+            System.out.println(res.size() - 1);
+            res.sort((Integer a, Integer b) ->  a > b ? 1 : - 1);
+            for(Integer i : res) {
+                System.out.print(i + " ");
+            } */
+
+        }
+    }
+}
--- GreedyAlgorithms/HuffmanCodes/HuffmanCode/HuffmanCode.java
+++ GreedyAlgorithms/HuffmanCodes/HuffmanCode/HuffmanCode.java
@@ -5,16 +5,28 @@ public class HuffmanCode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        char[] arr = str.toCharArray();
-        PriorityQueue queue = new PriorityQueue(str.length());
+        PriorityQueue<HuffmanNode> queue = new PriorityQueue(str.length());
         for (int i = 1; i <= str.length(); i++) {
             HuffmanNode node = new HuffmanNode();
             node.setI(i);
-            node.setF(arr[i]);
+            node.setF(str.charAt(i));
             queue.add(node);
         }
+        int size = 0;
         for (int k = str.length() + 1; k < 2*str.length() - 1; k++) {
-            //HuffmanNode node = queue.
+            HuffmanNode i = queue.poll();
+            HuffmanNode j = queue.poll();
+            HuffmanNode nodeK = new HuffmanNode();
+            nodeK.setI(k);
+            size = i.getF() + j.getF();
+            nodeK.setF(size);
+            queue.add(nodeK);
         }
+        System.out.println(queue.size() + " " + size);
+        StringBuilder encodedString = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            //encodedString.append(queue.)
+        }
+        System.out.println(encodedString.toString());
     }
 }
